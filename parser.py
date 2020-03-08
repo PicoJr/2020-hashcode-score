adefa9b45b2cabf25526f07cfc07485c510d629d
PicoJr <PicoJr@notanemail.com>
2020-03-08 17:28:22 +0100

    Fix lint issues

--- parser.py
+++ parser.py
@@ -1,9 +1,15 @@
+"""
+Hashcode 2020 data structures and parsing helpers
+"""
+
 from typing import List, Set, TextIO
-from io import TextIOWrapper
 
 
-class Library:
-    def __init__(self, id_: int, n_books: int, signup_delay: int, books_per_day: int, books: Set[int]):
+class Library:  # pylint: disable=too-few-public-methods
+    """Library"""
+
+    def __init__(self, id_: int,  # pylint: disable=too-many-arguments
+                 n_books: int, signup_delay: int, books_per_day: int, books: Set[int]):
         self.id_ = id_
         self.n_books = n_books
         self.signup_delay = signup_delay  # days
@@ -11,8 +17,11 @@ class Library:
         self.books = books  # books id set
 
 
-class InputDataSet:
-    def __init__(self, n_books, n_libraries, n_days, book_scores, libraries):
+class InputDataSet:  # pylint: disable=too-few-public-methods
+    """Input Data"""
+
+    def __init__(self, n_books,  # pylint: disable=too-many-arguments
+                 n_libraries, n_days, book_scores, libraries):
         self.n_books = n_books
         self.n_libraries = n_libraries
         self.n_days = n_days
@@ -23,14 +32,16 @@ class InputDataSet:
         return f"B: {self.n_books} L: {self.n_libraries} D: {self.n_days}"
 
 
-class LibraryOrder:
+class LibraryOrder:  # pylint: disable=too-few-public-methods
+    """Library Order"""
     def __init__(self, id_: int, n_books: int, books: List[int]):
         self.id_ = id_
         self.n_books = n_books
         self.books = books  # book id list (ordered)
 
 
-class OutputDataSet:
+class OutputDataSet:  # pylint: disable=too-few-public-methods
+    """Output Data"""
     def __init__(self, n_libraries, library_orders: List[LibraryOrder]):
         self.n_libraries = n_libraries
         self.library_orders = library_orders  # List[LibraryOrder]
@@ -40,27 +51,43 @@ class OutputDataSet:
 
 
 def parse_input_file(input_file) -> InputDataSet:
-    (B, L, D) = map(int, input_file.readline().split(' '))
+    """
+    Parse input file and return an initialized InputDataSet struct
+    :param input_file: file for parsing
+    :return: InputDataSet
+    """
+    # B, L, D
+    (total_books, total_libraries, total_days) = map(int, input_file.readline().split(' '))
     book_scores = list(map(int, input_file.readline().split(' ')))
     libraries = []
-    for id_ in range(L):
+    for id_ in range(total_libraries):
         (n_books, signup_delay, books_per_day) = map(int, input_file.readline().split(' '))
         books = set(map(int, input_file.readline().split(' ')))
         libraries.append(Library(id_, n_books, signup_delay, books_per_day, books))
-    return InputDataSet(B, L, D, book_scores, libraries)
+    return InputDataSet(total_books, total_libraries, total_days, book_scores, libraries)
 
 
 def parse_output_file(output_file) -> OutputDataSet:
-    L = int(output_file.readline())
+    """
+    Parse output file and return an initialized OutputDataSet struct
+    :param output_file: file for parsing
+    :return: OutputDataSet
+    """
+    n_orders = int(output_file.readline())  # L
     library_orders = []
-    for _ in range(L):
+    for _ in range(n_orders):
         (id_, n_books) = map(int, output_file.readline().split(' '))
         books = list(map(int, output_file.readline().split(' ')))
         library_orders.append(LibraryOrder(id_, n_books, books))
-    return OutputDataSet(L, library_orders)
+    return OutputDataSet(n_orders, library_orders)
 
 
 def write_output_file(output_data: OutputDataSet, output_file: TextIO):
+    """
+    Dump output data (solution) to output file
+    :param output_data: solution
+    :param output_file: file for dumping
+    """
     output_file.write(f"{output_data.n_libraries}\n")
     for library_order in output_data.library_orders:
         output_file.write(f"{library_order.id_} {library_order.n_books}\n")
@@ -68,6 +95,14 @@ def write_output_file(output_data: OutputDataSet, output_file: TextIO):
 
 
 def build_signup_schedule(input_data_set: InputDataSet, output_data_set: OutputDataSet):
+    """
+    Return signup schedule i.e (day signup complete, library id) list
+
+    Note: returned list in arbitrary order
+    :param input_data_set:
+    :param output_data_set:
+    :return: (day signup complete, library id) list
+    """
     day = 0
     signup_schedule = []
     for library_order in output_data_set.library_orders:
--- score.py
+++ score.py
@@ -1,16 +1,29 @@
+"""
+Compute score
+"""
+
 import argparse
 import logging
+from typing import Set
 
-from parser import InputDataSet, OutputDataSet, parse_input_file, parse_output_file, build_signup_schedule
+from parser import InputDataSet, OutputDataSet, parse_input_file, parse_output_file
+from parser import build_signup_schedule
 
 
-def compute_score(input_data_set: InputDataSet, output_data_set: OutputDataSet):
+def compute_score(input_data_set: InputDataSet, output_data_set: OutputDataSet) -> int:
+    """
+    Compute score
+    :param input_data_set: input
+    :param output_data_set: output
+    :return: score
+    """
     score = 0
-    books_already_sent = set()
+    books_already_sent: Set[int] = set()
     signup_schedule = build_signup_schedule(input_data_set, output_data_set)
-    library_orders = {library_order.id_: library_order for library_order in output_data_set.library_orders}
+    library_orders = {library_order.id_: library_order
+                      for library_order in output_data_set.library_orders}
     for (signup_day, signup_finished_id) in signup_schedule:
-        logging.debug(f"day {signup_day}: library {signup_finished_id} signup is finished")
+        logging.debug("day %s: library %s signup is finished", signup_day, signup_finished_id)
         days_left = input_data_set.n_days - signup_day
         if days_left <= 0:
             continue
@@ -19,7 +32,7 @@ def compute_score(input_data_set: InputDataSet, output_data_set: OutputDataSet):
         n_books_sent = days_left * books_per_day
         n_books_sent = min(n_books_sent, library_order.n_books)
         books_sent = library_order.books[:n_books_sent]
-        logging.debug(f"day {signup_day}: {n_books_sent} books sent: {books_sent}")
+        logging.debug("day %s: %s books sent: %s", signup_day, n_books_sent, books_sent)
         for book in books_sent:
             if book not in books_already_sent:
                 book_score = input_data_set.book_scores[book]
@@ -29,6 +42,7 @@ def compute_score(input_data_set: InputDataSet, output_data_set: OutputDataSet):
 
 
 def main():
+    """ main """
     parser = argparse.ArgumentParser(description="hashcode-2020 score")
     parser.add_argument("input", help="input file")
     parser.add_argument("output", help="output file")
